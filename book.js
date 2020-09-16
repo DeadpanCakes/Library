8e95e6667961c38b4efc2b1bc49aa12dd87ef4e0
Anthony Mendoza <anthony.mendoza310@gmail.com>
2020-09-15 17:24:12 -0700

    wrote a small constructor function as per the curriculum

---
+++ book.js
@@ -0,0 +1,8 @@
+/*
+write fn to construct book obj that accept and retain info about title author pagecount whether you have read the book and also a method that returns all this
+info in a concise string
+no interface
+input = title, author, pg, read or not
+output = obj
+write obj constructor
+*/
